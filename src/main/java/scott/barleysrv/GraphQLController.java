8f0774890a1ee69e71f7f4088ab1e0a8edb0af0f
Scott Sinclair <scottysinclair@gmail.com>
2019-04-14 16:50:49 +0200

    graphql post query and sdl in.

--- src/main/java/scott/barleysrv/GraphQLController.java
+++ src/main/java/scott/barleysrv/GraphQLController.java
@@ -6,10 +6,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import scott.barleydb.api.graphql.BarleyGraphQLSchema;
+import scott.barleydb.api.graphql.GraphQLContext;
+import scott.barleydb.api.graphql.GraphQLExecutionException;
 
 @RestController
 public class GraphQLController {
@@ -20,10 +24,33 @@ public class GraphQLController {
   private Map<String, BarleyGraphQLSchema> schemasByNamespace = new HashMap<>();
 
   @RequestMapping(path = "/sdl/{env}/{namespace}", produces = MediaType.TEXT_PLAIN_VALUE)
-  public String graphql(@PathVariable(value = "env") String envName, @PathVariable(value = "namespace") String namespace) {
+  public String sdl(@PathVariable(value = "env") String envName, @PathVariable(value = "namespace") String namespace) {
     BarleyGraphQLSchema schema = getOrCreate(envName, namespace);
     return schema.getSdlString();
   }
+
+  @RequestMapping(path = "/graphql/{env}/{namespace}",
+                  method = RequestMethod.POST,
+                  consumes = MediaType.APPLICATION_JSON_VALUE,
+                  produces = MediaType.APPLICATION_JSON_VALUE)
+  public Object query(
+      @PathVariable(value = "env") String envName,
+      @PathVariable(value = "namespace") String namespace,
+      @RequestBody Map<String,Object> body) {
+
+    BarleyGraphQLSchema schema = getOrCreate(envName, namespace);
+    GraphQLContext ctx = schema.newContext();
+    String query = (String)body.get("query");
+    try {
+      Object result = ctx.execute(query);
+      return result;
+    }
+    catch(GraphQLExecutionException x) {
+        return x.getErrors();
+    }
+  }
+
+
 
   private synchronized BarleyGraphQLSchema getOrCreate(String environmentName, String namespace) {
     BarleyGraphQLSchema schema = schemasByNamespace.get(namespace);
